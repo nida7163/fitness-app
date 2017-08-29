188448655da5fa13db27c34baf96d90bec7abc2b
Gerald Perriman <geraldperriman@gmail.com>
2017-08-28 20:35:39 -0400

    handlebars

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+-- @author: Thomas Thompson
+-- @github: tomtom28
+-- @comment: Homework 14 - Eat the Burger!
+
+
+-- Database Creation
+CREATE DATABASE exercise_db;
+USE exercise_db;
+-- Table Creation
+CREATE TABLE training(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+workout VARCHAR(50),
+sets INTEGER(2),
+rep1 INTEGER(3),
+rep2 INTEGER(3),
+rep3 INTEGER(3),
+KilledIt BOOLEAN,
+date TIMESTAMP);
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+-- @author: Thomas Thompson
+-- @github: tomtom28
+-- @comment: Homework 14 - Eat the Burger!
+
+
+INSERT INTO training(id,workout, sets, rep1, rep2, rep3, killed it, date)
+VALUES (id,"Dumbbell Bench Press", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Lat Pulldown", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Overhead Dumbbell Press", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Leg Press", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Lying Leg Curls", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Rope Press Down", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Barbell Bicep Curls", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Standing Calf Raise", 3, 8, 10, 12, false, "2016-11-05 04:20:00"),
+  (id,"Crunch", 3, 15, false, "2016-11-05 04:20:00");
